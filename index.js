70a201b89d901c55bb178e7a0dc8d526df6b7b1c
hiroshi375 <68233466+hiroshi375@users.noreply.github.com>
2026-06-29 20:48:55 +0900

    カメラ撮影時にアプリが落ちるのを改善

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import { registerRootComponent } from "expo";
 import "react-native-get-random-values";
+import "./src/lib/configureAmplify";
+
+import { registerRootComponent } from "expo";
 import App from "./App";
 
 registerRootComponent(App);
